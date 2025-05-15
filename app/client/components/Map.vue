721dd8a0295d975c9b7a4921c92c63f465f69261
mat-afk <mateusc.ramos@outlook.com>
2025-05-15 17:36:14 -0300

    refactor: add map props and temporary icons

--- app/client/components/Map.vue
+++ app/client/components/Map.vue
@@ -1,12 +1,20 @@
+<script lang="ts" setup>
+interface Props {
+  zoom: number;
+  center: number[];
+}
+const props = defineProps<Props>();
+</script>
+
 <template>
   <LMap
     class="min-w-screen min-h-screen"
-    :zoom="12"
-    :center="[-23.5489, -46.6388]"
+    :zoom="props.zoom"
+    :center="props.center"
     :use-global-leaflet="false"
   >
     <LTileLayer
-      url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       layer-type="base"
       name="OpenStreetMap"
--- app/client/pages/index.vue
+++ app/client/pages/index.vue
@@ -1,13 +1,28 @@
 <script lang="ts" setup>
 import { locations } from "~/data";
+
+const zoom = ref(12);
+const center = ref([-23.5489, -46.6388]);
+
+const handleZoom = (location: { latitude: number; longitude: number }) => {
+  console.log("safasdfs");
+  zoom.value = 5;
+  center.value = [location.latitude, location.longitude];
+};
 </script>
 
 <template>
-  <Map>
+  <Map :zoom="zoom" :center="center">
     <Marker
       v-for="location in locations"
+      @click="handleZoom"
       :latitude="location.latitude"
       :longitude="location.longitude"
-    ></Marker>
+      :icon="{
+        url: 'https://static.thenounproject.com/png/335079-200.png',
+        width: 20,
+        height: 20,
+      }"
+    />
   </Map>
 </template>
