d29ee4ceeac610f260126d69e1c32ff2dfa05f44
Mateus <mateusc.ramos@outlook.com>
2025-05-17 13:46:54 -0300

    [client] feat: add zoom to marker functionality

--- app/client/pages/index.vue
+++ app/client/pages/index.vue
@@ -1,28 +1,31 @@
 <script lang="ts" setup>
 import { locations } from "~/data";
 
+const map = ref(null);
 const zoom = ref(12);
 const center = ref([-23.5489, -46.6388]);
 
-const handleZoom = (location: { latitude: number; longitude: number }) => {
-  console.log("safasdfs");
-  zoom.value = 5;
-  center.value = [location.latitude, location.longitude];
+const zoomToMarker = (location: { latitude: number; longitude: number }) => {
+  map.value?.flyTo([location.latitude, location.longitude], 18, {
+    animate: true,
+    duration: 1,
+  });
 };
 </script>
 
 <template>
-  <Map :zoom="zoom" :center="center">
+  <Map ref="map" :zoom="zoom" :center="center">
     <Marker
       v-for="location in locations"
-      @click="handleZoom"
+      @click="() => zoomToMarker(location)"
+      :key="location.name"
       :latitude="location.latitude"
       :longitude="location.longitude"
       :icon="{
-        url: 'https://static.thenounproject.com/png/335079-200.png',
         width: 20,
         height: 20,
       }"
     />
+    <LMarker :lat-lng="center"></LMarker>
   </Map>
 </template>
