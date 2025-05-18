60fd38485025bcbc9ecf411c5058bcff66800c96
Mateus <mateusc.ramos@outlook.com>
2025-05-18 19:28:08 -0300

    [api] feat: setup Spring Boot project

---
+++ app/api/src/main/java/br/app/vizo/VizoApplication.java
@@ -0,0 +1,13 @@
+package br.app.vizo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizoApplication.class, args);
+	}
+
+}
