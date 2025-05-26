5fde52cfaf0bb5a9bcf31a02d808acf636d6e920
Mateus <mateusc.ramos@outlook.com>
2025-05-26 13:45:12 -0300

    [api] feat: add exception handling for unauthorized requests

--- app/api/src/main/java/br/app/vizo/config/security/SecurityConfig.java
+++ app/api/src/main/java/br/app/vizo/config/security/SecurityConfig.java
@@ -1,18 +1,26 @@
 package br.app.vizo.config.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +44,20 @@ public class SecurityConfig {
                             .permitAll()
                         .anyRequest()
                             .authenticated()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.authenticationEntryPoint(new AuthenticationEntryPoint() {
+                            @Override
+                            public void commence(
+                                    HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    AuthenticationException authException
+                            ) throws IOException, ServletException {
+                                response.setStatus(401);
+                                response.setContentType("application/json");
+                                response.getWriter().write("{ \"error\": \"%s\" }".formatted(authException.getMessage()));
+                            }
+                        })
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
