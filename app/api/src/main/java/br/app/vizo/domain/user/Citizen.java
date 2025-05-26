ae5954fc13373c103c8eec471c8998ceda520bc4
Mateus <mateusc.ramos@outlook.com>
2025-05-26 12:27:25 -0300

    [api] refactor: update credibility points of citizen from Long to Double

--- app/api/src/main/java/br/app/vizo/domain/user/Citizen.java
+++ app/api/src/main/java/br/app/vizo/domain/user/Citizen.java
@@ -15,21 +15,21 @@ public class Citizen extends User {
     private String name;
 
     @Column(name = "credibility_points")
-    private Long credibilityPoints;
+    private Double credibilityPoints;
 
     public Citizen() {
         super();
         this.name = "";
-        this.credibilityPoints = 0L;
+        this.credibilityPoints = 1.0;
     }
 
-    public Citizen(String document, String email, String password, String name, Long credibilityPoints) {
+    public Citizen(String document, String email, String password, String name, Double credibilityPoints) {
         super(document, email, password);
         this.name = name;
         this.credibilityPoints = credibilityPoints;
     }
 
     public Citizen(String document, String email, String password, String name) {
-        this(document, email, password, name, 0L);
+        this(document, email, password, name, 1.0);
     }
 }
