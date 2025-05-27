e7c9ffa7c996039959f24fba0fe3e6d88213ea94
mat-afk <mateusc.ramos@outlook.com>
2025-05-27 14:18:18 -0300

    [client] feat: add NavMenu

---
+++ app/client/components/nav/NavItem.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+interface Props {
+  icon?: string;
+  label?: string;
+  selected?: boolean;
+}
+const props = defineProps<Props>();
+
+const icon = ref(`tabler:${props.icon ?? "favicon"}`);
+</script>
+
+<template>
+  <section
+    class="p-2 flex flex-row items-center gap-2 pl-5 rounded-e-full transition cursor-pointer"
+    :class="selected ? 'bg-blue-700 text-zinc-50' : 'hover:bg-zinc-100'"
+  >
+    <Icon :name="icon" mode="svg" size="1.9rem" />
+    <span>{{ props.label ?? "Label" }}</span>
+  </section>
+</template>
---
+++ app/client/components/nav/NavMenu.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+interface NavOption {
+  id: string;
+  icon: string;
+  label: string;
+}
+
+const navOptions: NavOption[] = [
+  { id: "home", icon: "apps", label: "Início" },
+  { id: "report", icon: "message-report", label: "Reportar" },
+  { id: "settings", icon: "settings", label: "Configurações" },
+];
+const selectedItem = ref<NavOption["id"]>("home");
+</script>
+
+<template>
+  <aside
+    class="flex flex-col w-[20%] justify-between pr-3 text-[1.2rem] text-zinc-950"
+  >
+    <header class="h-[15%] flex justify-center items-center">Vizo</header>
+    <main class="h-full flex flex-col gap-2 py-3">
+      <NavItem
+        v-for="option in navOptions"
+        :selected="selectedItem === option.id"
+        @click="() => (selectedItem = option.id)"
+        :icon="option.icon"
+        :label="option.label"
+      />
+    </main>
+    <footer class="h-[15%]">
+      <NavItem icon="transfer-out" label="Sair" />
+    </footer>
+  </aside>
+</template>
