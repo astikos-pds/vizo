6e2e1732df1735a1c7550a552e74c7ae547225a0
Mateus <mateusc.ramos@outlook.com>
2025-06-01 23:41:16 -0300

    [api] feat: add CORS configuration

---
+++ app/api/src/main/java/br/app/vizo/config/security/CorsConfig.java
@@ -0,0 +1,29 @@
+package br.app.vizo.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${client.url}")
+    private String CLIENT_URL;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(CLIENT_URL);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
