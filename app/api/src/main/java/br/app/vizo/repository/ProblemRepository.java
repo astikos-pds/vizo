596a489aabfde0a69da4fd91f0099012522824ce
Mateus <mateusc.ramos@outlook.com>
2025-07-02 01:10:19 -0300

    [api] refactor: change return type of findAllByValidated from Page to List

--- app/api/src/main/java/br/app/vizo/repository/ProblemRepository.java
+++ app/api/src/main/java/br/app/vizo/repository/ProblemRepository.java
@@ -1,12 +1,12 @@
 package br.app.vizo.repository;
 
 import br.app.vizo.domain.problem.Problem;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,5 +27,5 @@ public interface ProblemRepository extends JpaRepository<Problem, UUID> {
             @Param("lon") Double longitude,
             @Param("distance") Double distance);
 
-    Page<Problem> findAllByValidated(Boolean validated);
+    List<Problem> findAllByValidated(Boolean validated);
 }
