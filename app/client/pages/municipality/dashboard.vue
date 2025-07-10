cc9cbedf7d5c2628627bff2cdf3e79c1d135d2c6
Rodrigo <rodrigobaesa@gmail.com>
2025-07-10 09:53:23 -0300

    [client] feat: adding charts to admin page

---
+++ app/client/pages/municipality/dashboard.vue
@@ -0,0 +1,417 @@
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import DropdownMenu from '@/common/DropdownMenu.vue'
+import VueApexCharts from 'vue3-apexcharts'
+
+const { t } = useI18n();
+
+definePageMeta({
+  layout: "admin",
+});
+
+const menuItems = [
+  { label: 'View More', onClick: () => console.log('View More clicked') },
+  { label: 'Delete', onClick: () => console.log('Delete clicked') },
+]
+
+const series = ref([
+  {
+    name: 'afdafafafaf',
+    data: [168, 385, 201, 298, 187, 195, 291, 110, 215, 390, 280, 112],
+  },
+])
+
+const chartOptions = ref({
+  colors: ['#465fff'],
+  chart: {
+    fontFamily: 'Outfit, sans-serif',
+    type: 'bar',
+    toolbar: {
+      show: false,
+    },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '39%',
+      borderRadius: 5,
+      borderRadiusApplication: 'end',
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    show: true,
+    width: 4,
+    colors: ['transparent'],
+  },
+  xaxis: {
+    categories: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  legend: {
+    show: true,
+    position: 'top',
+    horizontalAlign: 'left',
+    fontFamily: 'Outfit',
+    markers: {
+      radius: 99,
+    },
+  },
+  yaxis: {
+    title: false,
+    tickAmount: 4,
+  },
+  grid: {
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  fill: {
+    opacity: 1,
+  },
+    tooltip: {
+    x: {
+      show: false,
+    },
+    y: {
+      formatter: function (val: number) {
+        return val.toString()
+      },
+    },
+  },
+})
+
+onMounted(() => {
+})
+
+const problems = ref([
+  {
+    name: 'Buraco',
+    variants: 2,
+    neighborhood: 'Belém',
+    status: 'Resolvido',
+  },
+  {
+    name: 'Buraco',
+    variants: 1,
+    neighborhood: 'Brás',
+    status: 'Em Andamento',
+  },
+  {
+    name: 'Árvore Caída',
+    variants: 2,
+    neighborhood: 'Parque São Vicente',
+    status: 'Resolvido',
+  },
+  {
+    name: 'Enxente',
+    variants: 2,
+    neighborhood: 'Canindé',
+    status: 'Em Análise',
+  },
+  {
+    name: 'Semáforo Quebrado',
+    variants: 1,
+    neighborhood: 'Canindé',
+    status: 'Resolvido',
+  },
+])
+
+const options = [
+  { value: 'optionOne', label: 'Monthly' },
+  { value: 'optionTwo', label: 'Quarterly' },
+  { value: 'optionThree', label: 'Annually' },
+]
+
+const seriess = ref([
+  {
+    name: 'Problemas',
+    data: [180, 190, 170, 160, 175, 165, 170, 205, 230, 210, 240, 235],
+  },
+  {
+    name: 'Problemas Resolvidos',
+    data: [40, 30, 50, 40, 55, 40, 70, 100, 110, 120, 150, 140],
+  },
+])
+
+const chartOptionss = ref({
+  legend: {
+    show: false,
+    position: 'top',
+    horizontalAlign: 'left',
+  },
+  colors: ['#465FFF', '#9CB9FF'],
+  chart: {
+    fontFamily: 'Outfit, sans-serif',
+    type: 'area',
+    toolbar: {
+      show: false,
+    },
+  },
+  fill: {
+    gradient: {
+      enabled: true,
+      opacityFrom: 0.55,
+      opacityTo: 0,
+    },
+  },
+  stroke: {
+    curve: 'straight',
+    width: [2, 2],
+  },
+  markers: {
+    size: 0,
+  },
+  labels: {
+    show: false,
+    position: 'top',
+  },
+  grid: {
+    xaxis: {
+      lines: {
+        show: false,
+      },
+    },
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    x: {
+      format: 'dd MMM yyyy',
+    },
+  },
+  xaxis: {
+    type: 'category',
+    categories: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  yaxis: {
+    title: {
+      style: {
+        fontSize: '0px',
+      },
+    },
+  },
+})
+
+const selected = ref('optionOne')
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-6 p-8 md:grid-cols-2">
+    <div
+      class="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6"
+    >
+      <div class="flex items-center justify-between">
+        <h3 class="text-lg font-semibold text-gray-800 dark:text-white/90">Incidência de Problemas</h3>
+        <div class="relative h-fit">
+          <DropdownMenu :menu-items="menuItems">
+            <template #icon>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M10.2441 6C10.2441 5.0335 11.0276 4.25 11.9941 4.25H12.0041C12.9706 4.25 13.7541 5.0335 13.7541 6C13.7541 6.9665 12.9706 7.75 12.0041 7.75H11.9941C11.0276 7.75 10.2441 6.9665 10.2441 6ZM10.2441 18C10.2441 17.0335 11.0276 16.25 11.9941 16.25H12.0041C12.9706 16.25 13.7541 17.0335 13.7541 18C13.7541 18.9665 12.9706 19.75 12.0041 19.75H11.9941C11.0276 19.75 10.2441 18.9665 10.2441 18ZM11.9941 10.25C11.0276 10.25 10.2441 11.0335 10.2441 12C10.2441 12.9665 11.0276 13.75 11.9941 13.75H12.0041C12.9706 13.75 13.7541 12.9665 13.7541 12C13.7541 11.0335 12.9706 10.25 12.0041 10.25H11.9941Z" fill="currentColor" />
+              </svg>
+            </template>
+          </DropdownMenu>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <VueApexCharts type="bar" height="200" :options="chartOptions" :series="series" />
+      </div>
+    </div>
+
+    <div
+    class="rounded-2xl border border-gray-200 bg-white px-5 pb-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6"
+  >
+    <div class="flex flex-col gap-5 mb-6 sm:flex-row sm:justify-between">
+      <div class="w-full">
+        <h3 class="text-lg font-semibold text-gray-800 dark:text-white/90">Problemas Resolvidos</h3>
+        <p class="mt-1 text-gray-500 text-theme-sm dark:text-gray-400">
+          Relação de problemas resolvidos nos ultimos dias.
+        </p>
+      </div>
+
+      <div class="relative">
+        <div class="inline-flex items-center gap-0.5 rounded-lg bg-gray-100 p-0.5 dark:bg-gray-900">
+          <button
+            v-for="option in options"
+            :key="option.value"
+            @click="selected = option.value"
+            :class="[
+              selected === option.value
+                ? 'shadow-theme-xs text-gray-900 dark:text-white bg-white dark:bg-gray-800'
+                : 'text-gray-500 dark:text-gray-400',
+              'px-3 py-2 font-medium rounded-md text-theme-sm hover:text-gray-900 hover:shadow-theme-xs dark:hover:bg-gray-800 dark:hover:text-white',
+            ]"
+          >
+            {{ option.label }}
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="max-w-full overflow-x-auto custom-scrollbar">
+      <div id="chartThree" class="-ml-4 min-w-[1000px] xl:min-w-full pl-2">
+        <VueApexCharts type="area" height="200" :options="chartOptionss" :series="seriess"/>
+      </div>
+    </div>
+  </div>
+
+  <div class="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
+      <div class="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
+        <div>
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-white/90">Últimos Problemas</h3>
+        </div>
+
+        <div class="flex items-center gap-3">
+          <button class="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
+            <svg
+              class="stroke-current fill-white dark:fill-gray-800"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.29004 5.90393H17.7067"
+                stroke=""
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M17.7075 14.0961H2.29085"
+                stroke=""
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M12.0826 3.33331C13.5024 3.33331 14.6534 4.48431 14.6534 5.90414C14.6534 7.32398 13.5024 8.47498 12.0826 8.47498C10.6627 8.47498 9.51172 7.32398 9.51172 5.90415C9.51172 4.48432 10.6627 3.33331 12.0826 3.33331Z"
+                fill=""
+                stroke=""
+                stroke-width="1.5"
+              />
+              <path
+                d="M7.91745 11.525C6.49762 11.525 5.34662 12.676 5.34662 14.0959C5.34661 15.5157 6.49762 16.6667 7.91745 16.6667C9.33728 16.6667 10.4883 15.5157 10.4883 14.0959C10.4883 12.676 9.33728 11.525 7.91745 11.525Z"
+                fill=""
+                stroke=""
+                stroke-width="1.5"
+              />
+            </svg>
+
+            Filtrar
+          </button>
+
+          <button class="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
+            Ver Todos
+          </button>
+        </div>
+      </div>
+
+      <div class="max-w-full overflow-x-auto custom-scrollbar">
+        <table class="min-w-full">
+          <thead>
+            <tr class="border-t border-gray-100 dark:border-gray-800">
+              <th class="py-3 text-left">
+                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">Problema</p>
+              </th>
+              <th class="py-3 text-left">
+                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">Bairro</p>
+              </th>
+              <th class="py-3 text-left">
+                <p class="font-medium text-gray-500 text-theme-xs dark:text-gray-400">Status</p>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(problem, index) in problems"
+              :key="index"
+              class="border-t border-gray-100 dark:border-gray-800">
+              <td class="py-3 whitespace-nowrap">
+                <div class="flex items-center gap-3">
+                  <div>
+                    <p class="font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                      {{ problem.name }}
+                    </p>
+                    <span class="text-gray-500 text-theme-xs dark:text-gray-400"
+                      >{{ problem.variants }} Reportes</span
+                    >
+                  </div>
+                </div>
+              </td>
+              <td class="py-3 whitespace-nowrap">
+                <p class="text-gray-500 text-theme-sm dark:text-gray-400">{{ problem.neighborhood }}</p>
+              </td>
+              <td class="py-3 whitespace-nowrap">
+                <span
+                  :class="{
+                    'rounded-full px-2 py-0.5 text-theme-xs font-medium': true,
+                    'bg-success-50 text-success-600 dark:bg-success-500/15 dark:text-success-500':
+                      problem.status === 'Resolvido',
+                    'bg-warning-50 text-warning-600 dark:bg-warning-500/15 dark:text-orange-400':
+                      problem.status === 'Em Andamento',
+                    'bg-error-50 text-error-600 dark:bg-error-500/15 dark:text-error-500':
+                      problem.status === 'Em Análise',
+                  }"
+                >
+                  {{ problem.status }}
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
