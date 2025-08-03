c6e0f92a23a22800125c471fdb06665e66e9f9b2
Mateus <mateusc.ramos@outlook.com>
2025-08-03 20:21:12 -0300

    [api] feat: add LeaveDepartmentUseCase

---
+++ app/api/src/main/java/br/app/vizo/application/usecase/assignment/LeaveDepartmentUseCase.java
@@ -0,0 +1,25 @@
+package br.app.vizo.application.usecase.assignment;
+
+import br.app.vizo.application.UseCase;
+import br.app.vizo.application.service.AuthorizationService;
+import br.app.vizo.core.affiliation.AffiliatedUser;
+import br.app.vizo.core.assignment.AssignedUser;
+import br.app.vizo.core.assignment.AssignedUserRepository;
+import br.app.vizo.core.user.User;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@UseCase
+@RequiredArgsConstructor
+public class LeaveDepartmentUseCase {
+
+    private final AuthorizationService authorizationService;
+    private final AssignedUserRepository assignedUserRepository;
+
+    public void execute(User loggedInUser, UUID municipalityId, UUID departmentId) {
+        AssignedUser assignedUser = this.authorizationService.ensureUserIsAssignedTo(loggedInUser, municipalityId, departmentId);
+
+        this.assignedUserRepository.deleteById(assignedUser.getId());
+    }
+}
