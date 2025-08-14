46dbb3fff8b40a140bd2d6ecde8b87aeca96875d
Mateus <mateusc.ramos@outlook.com>
2025-08-14 16:23:14 -0300

    [api] feat: add ProblemController

--- app/api/src/main/java/br/app/vizo/application/dto/page/PaginationDTO.java
+++ app/api/src/main/java/br/app/vizo/application/dto/page/PaginationDTO.java
@@ -2,15 +2,21 @@ package br.app.vizo.application.dto.page;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 public record PaginationDTO(
-        int page,
-        int size
+        Integer page,
+        Integer size
 ) {
 
     public static Pageable resolve(PaginationDTO paginationDTO) {
-        return paginationDTO == null
-                ? PageRequest.of(0, 20)
-                : PageRequest.of(paginationDTO.page(), paginationDTO.size());
+        if (paginationDTO == null) {
+            return PageRequest.of(0, 20, Sort.by(Sort.Direction.ASC, "createdAt"));
+        }
+
+        int page = paginationDTO.page() == null ? 0 : paginationDTO.page();
+        int size = paginationDTO.size() == null ? 20 : paginationDTO.size();
+
+        return PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
     }
 }
