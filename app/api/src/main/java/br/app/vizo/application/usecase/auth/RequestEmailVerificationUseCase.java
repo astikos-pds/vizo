53f2e880bb6e0734b5e1168c384db3d95aa60a5b
mat-afk <mateusc.ramos@outlook.com>
2025-09-09 01:31:13 -0300

    [api] feat: add duplicate institutional e-mail validation for affiliation e-mail verification

--- app/api/src/main/java/br/app/vizo/application/usecase/auth/RequestEmailVerificationUseCase.java
+++ app/api/src/main/java/br/app/vizo/application/usecase/auth/RequestEmailVerificationUseCase.java
@@ -3,10 +3,12 @@ package br.app.vizo.application.usecase.auth;
 import br.app.vizo.application.UseCase;
 import br.app.vizo.application.dto.EmailVerificationDTO;
 import br.app.vizo.application.exception.EmailAlreadyInUseException;
+import br.app.vizo.application.exception.InstitutionalEmailAlreadyInUse;
 import br.app.vizo.application.exception.auth.UserNotFoundException;
 import br.app.vizo.application.mapper.EmailVerificationRequestMapper;
 import br.app.vizo.application.service.EmailService;
 import br.app.vizo.application.usecase.auth.request.EmailRequestDTO;
+import br.app.vizo.core.affiliation.AffiliatedUserRepository;
 import br.app.vizo.core.user.UserRepository;
 import br.app.vizo.core.verification.EmailVerificationRequest;
 import br.app.vizo.core.verification.EmailVerificationRequestFactory;
@@ -19,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class RequestEmailVerificationUseCase {
 
     private final UserRepository userRepository;
+    private final AffiliatedUserRepository affiliatedUserRepository;
     private final EmailVerificationRequestRepository emailVerificationRequestRepository;
     private final EmailVerificationRequestFactory emailVerificationRequestFactory;
     private final EmailVerificationRequestMapper emailVerificationRequestMapper;
@@ -34,6 +37,13 @@ public class RequestEmailVerificationUseCase {
 
         if (body.purpose() == VerificationPurpose.PASSWORD_CHANGE) {
             this.userRepository.findByEmail(body.email()).orElseThrow(UserNotFoundException::new);
+        }
+
+        if (body.purpose() == VerificationPurpose.AFFILIATION) {
+            boolean institutionalEmailAlreadyInUse = this.affiliatedUserRepository.existsByInstitutionalEmail(body.email());
+            if (institutionalEmailAlreadyInUse) {
+                throw new InstitutionalEmailAlreadyInUse();
+            }
         }
 
         EmailVerificationRequest emailVerificationRequest = this.emailVerificationRequestRepository
