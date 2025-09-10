a58c207b555f14ff0d8f96d8860b564f3d44afd2
mat-afk <mateusc.ramos@outlook.com>
2025-09-10 02:35:14 -0300

    [api] feat: add email domain toleration for embedded domains in municipality

--- app/api/src/main/java/br/app/vizo/infrastructure/persistence/jpa/repository/MunicipalityJpaRepository.java
+++ app/api/src/main/java/br/app/vizo/infrastructure/persistence/jpa/repository/MunicipalityJpaRepository.java
@@ -2,6 +2,8 @@ package br.app.vizo.infrastructure.persistence.jpa.repository;
 
 import br.app.vizo.infrastructure.persistence.jpa.entity.MunicipalityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,6 @@ import java.util.UUID;
 @Repository
 public interface MunicipalityJpaRepository extends JpaRepository<MunicipalityEntity, UUID> {
 
-    Optional<MunicipalityEntity> findByEmailDomain(String emailDomain);
+    @Query(value = "SELECT * FROM municipalities m WHERE :email_domain LIKE CONCAT('%', m.email_domain)", nativeQuery = true)
+    Optional<MunicipalityEntity> findByEmailDomain(@Param("email_domain") String emailDomain);
 }
