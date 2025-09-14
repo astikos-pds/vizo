251db85abe60f3af2afa14aafb517ad9c0e223f0
mat-afk <mateusc.ramos@outlook.com>
2025-09-13 21:59:33 -0300

    [client] feat: change gradient colors

--- app/client/components/LogoGradient.vue
+++ app/client/components/LogoGradient.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="hidden lg:min-w-[45%] xl:min-w-[50%] h-full lg:flex justify-center items-center bg-linear-to-tr from-primary to-neutral-200 dark:to-neutral-500"
+    class="hidden lg:min-w-[45%] xl:min-w-[50%] h-full lg:flex justify-center items-center bg-linear-to-tr from-primary to-neutral-200"
   >
     <NuxtImg src="/favicon.svg" class="size-[40%]" alt="Logo" preload />
   </section>
